7c8bd2fa46f0ed16c410d129af8db1b37ec200b6
KhanWarden <baryrhan2004@gmail.com>
2025-04-06 19:53:04 +0500

    Change converting WON exchange rate

--- app/parsers/won_parser.py
+++ app/parsers/won_parser.py
@@ -12,5 +12,5 @@ class WonParser(CurrencyParser):
     @classmethod
     def get_exchange_rate(cls, rates: dict) -> float:
         usd_to_krw = float(rates.get("quotes", {}).get("USDKRW", 0))
-        converted_rate = usd_to_krw + 6
+        converted_rate = usd_to_krw + 5
         return round(converted_rate, 2)
