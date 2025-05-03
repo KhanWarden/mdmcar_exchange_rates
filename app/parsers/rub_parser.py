40c99ae34fb254b50ae434d0f0953fbb043e86e4
KhanWarden <baryrhan2004@gmail.com>
2025-05-03 13:06:30 +0500

    Берется "Продажа" курса рубля

--- app/parsers/rub_parser.py
+++ app/parsers/rub_parser.py
@@ -12,6 +12,5 @@ class RubParser(CurrencyParser):
         api_url = "https://api-app.sovcombank.ru/v1/currency/7700000000000"
         response = requests.get(api_url)
         json = response.json()
-        usd_to_rub = float(json.get("usd").get("buy"))
-        converted_rate = usd_to_rub + 2
-        return round(converted_rate, 2)
+        usd_to_rub = float(json.get("usd").get("sell"))
+        return round(usd_to_rub, 2)
